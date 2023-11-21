63979b0b5882fad85287d2c8b1d95da00f267c19
DanielaBaicoianu <114957081+DanielaBaicoianu@users.noreply.github.com>
2023-11-21 18:22:19 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE if not exists `laborator8_db`.`users`(
+    `id`              BIGINT      NOT NULL AUTO_INCREMENT,
+    `username`        VARCHAR(45) NOT NULL,
+    `full_name`       VARCHAR(45) NULL,
+    `user_type`       VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+    );
+
+CREATE TABLE if not exists `laborator8_db`.`user_details`(
+    `id`                BIGINT      NOT NULL AUTO_INCREMENT,
+    `user_id`        BIGINT NOT NULL,
+    `cnp`               VARCHAR(45) NOT NULL,
+    `age`               INT NULL,
+    `other_information` VARCHAR(255) NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `laborator8_db`.`users` (`id`)
+    );
